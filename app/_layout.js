1e26577007dfb2e788d978d5b6c8ad92fab1611a
miguel <gomez_miguelomar@yahoo.com>
2023-12-05 10:16:55 -0800

    Merge install-esLint-prettier.configs

--- app/_layout.js
+++ app/_layout.js
@@ -2,7 +2,7 @@ import { Stack } from 'expo-router/stack'
 import React from 'react'
 
 const Layout = () => {
-  return <Stack />
+	return <Stack />
 }
 
 export default Layout
--- app/index.js
+++ app/index.js
@@ -2,5 +2,5 @@ import { Redirect } from 'expo-router'
 import React from 'react'
 
 export default function Index() {
-  return <Redirect href="/home" />
+	return <Redirect href="/home" />
 }
