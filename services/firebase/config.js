40564d72a1fbdf9fed4e2978ef39e1c8bbcec41d
Iyiola <48067557+ialoko@users.noreply.github.com>
2023-12-16 14:57:49 -0500

    Update config.js
    Fixed the issues with the formatting with prettier

--- services/firebase/config.js
+++ services/firebase/config.js
@@ -1,4 +1,3 @@
-/* eslint-disable prettier/prettier */
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -9,22 +8,20 @@ import * as firebase from 'firebase';
 import '@firebase/auth';
 import '@firebase/firestore';
 
-
 require('dotenv').config();
 
-
 const firebaseConfig = {
-    apiKey: process.env.API_KEY,
-    authDomain: process.env.AUTH_DOMAIN,
-    projectId: "dfg-sapse",
-    storageBucket: "dfg-sapse.appspot.com",
-    messagingSenderId: "140818285182",
-    appId: "1:140818285182:web:bfbc2a7133a968977d4e2f",
-    measurementId: "G-Z0XMDWF8HB"
+	apiKey: process.env.API_KEY,
+	authDomain: process.env.AUTH_DOMAIN,
+	projectId: 'dfg-sapse',
+	storageBucket: 'dfg-sapse.appspot.com',
+	messagingSenderId: '140818285182',
+	appId: '1:140818285182:web:bfbc2a7133a968977d4e2f',
+	measurementId: 'G-Z0XMDWF8HB',
 };
 
 if (!firebase.apps.length) {
-    const app = firebase.initializeApp(firebaseConfig);
+	const app = firebase.initializeApp(firebaseConfig);
 }
 
 export { firebase };
