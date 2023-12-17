65cf2ea9eabf4b276c4448d0709dc88aa76f7f1f
Iyiola <48067557+ialoko@users.noreply.github.com>
2023-12-17 01:00:06 -0500

    Implemented firebase authentication

---
+++ app/LoginScreen.js
@@ -0,0 +1,139 @@
+/* eslint-disable no-alert */
+import React, { useState } from 'react';
+import { Image, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
+import { StyleSheet } from 'react-native';
+import { firestore, auth } from '../services/firebase/config';
+import { signInWithEmailAndPassword } from 'firebase/auth';
+import { doc, getDoc } from 'firebase/firestore';
+
+export default function LoginScreen({ navigation }) {
+	const [email, setEmail] = useState('');
+	const [password, setPassword] = useState('');
+
+	const onFooterLinkPress = () => {
+		navigation.navigate('Registration');
+	};
+
+	const onLoginPress = () => {
+		signInWithEmailAndPassword(auth, email, password)
+			.then(async (response) => {
+				const uid = response.user.uid;
+				const usersRef = doc(firestore, 'users', uid);
+				const userSnap = await getDoc(usersRef);
+				if (userSnap.exists()) {
+					const user = userSnap.data();
+					console.log('Document data:', user);
+					navigation.navigate('Home', { user });
+				} else {
+					// docSnap.data() will be undefined in this case
+					alert('User does not exist anymore.');
+				}
+			})
+			.catch((error) => {
+				alert(error);
+			});
+	};
+
+	return (
+		<View style={styles.container}>
+			<KeyboardAwareScrollView
+				style={{ flex: 1, width: '100%' }}
+				keyboardShouldPersistTaps="always"
+			>
+				<Image style={styles.logo} />
+				<TextInput
+					style={styles.input}
+					placeholder="E-mail"
+					placeholderTextColor="#aaaaaa"
+					onChangeText={(text) => setEmail(text)}
+					value={email}
+					underlineColorAndroid="transparent"
+					autoCapitalize="none"
+				/>
+				<TextInput
+					style={styles.input}
+					placeholderTextColor="#aaaaaa"
+					secureTextEntry
+					placeholder="Password"
+					onChangeText={(text) => setPassword(text)}
+					value={password}
+					underlineColorAndroid="transparent"
+					autoCapitalize="none"
+				/>
+				<TouchableOpacity
+					style={styles.button}
+					onPress={() => onLoginPress()}
+				>
+					<Text style={styles.buttonTitle}>Log in</Text>
+				</TouchableOpacity>
+				<View style={styles.footerView}>
+					<Text style={styles.footerText}>
+						Don't have an account?{' '}
+						<Text
+							onPress={onFooterLinkPress}
+							style={styles.footerLink}
+						>
+							Sign up
+						</Text>
+					</Text>
+				</View>
+			</KeyboardAwareScrollView>
+		</View>
+	);
+}
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		alignItems: 'center',
+	},
+	title: {},
+	logo: {
+		flex: 1,
+		height: 120,
+		width: 90,
+		alignSelf: 'center',
+		margin: 30,
+	},
+	input: {
+		height: 48,
+		borderRadius: 5,
+		overflow: 'hidden',
+		backgroundColor: 'white',
+		marginTop: 10,
+		marginBottom: 10,
+		marginLeft: 30,
+		marginRight: 30,
+		paddingLeft: 16,
+	},
+	button: {
+		backgroundColor: '#788eec',
+		marginLeft: 30,
+		marginRight: 30,
+		marginTop: 20,
+		height: 48,
+		borderRadius: 5,
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+	buttonTitle: {
+		color: 'white',
+		fontSize: 16,
+		fontWeight: 'bold',
+	},
+	footerView: {
+		flex: 1,
+		alignItems: 'center',
+		marginTop: 20,
+	},
+	footerText: {
+		fontSize: 16,
+		color: '#2e2e2d',
+	},
+	footerLink: {
+		color: '#788eec',
+		fontWeight: 'bold',
+		fontSize: 16,
+	},
+});
--- services/firebase/config.js
+++ services/firebase/config.js
@@ -1,30 +1,28 @@
-/* eslint-disable prettier/prettier */
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
-import * as firebase from 'firebase';
-import '@firebase/auth';
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
 import '@firebase/firestore';
-
-
-require('dotenv').config();
-
+import { getFirestore} from 'firebase/firestore';
+import { API_KEY, AUTH_DOMAIN } from '@env';
 
 const firebaseConfig = {
-    apiKey: process.env.API_KEY,
-    authDomain: process.env.AUTH_DOMAIN,
-    projectId: "dfg-sapse",
-    storageBucket: "dfg-sapse.appspot.com",
-    messagingSenderId: "140818285182",
-    appId: "1:140818285182:web:bfbc2a7133a968977d4e2f",
-    measurementId: "G-Z0XMDWF8HB"
+	apiKey: API_KEY,
+	authDomain: AUTH_DOMAIN,
+	projectId: 'dfg-sapse',
+	storageBucket: 'dfg-sapse.appspot.com',
+	messagingSenderId: '140818285182',
+	appId: '1:140818285182:web:bfbc2a7133a968977d4e2f',
+	measurementId: 'G-Z0XMDWF8HB',
 };
 
-if (!firebase.apps.length) {
-    const app = firebase.initializeApp(firebaseConfig);
-}
+const app = initializeApp(firebaseConfig);
+export const firestore = getFirestore(app);
+export const auth = getAuth();
 
-export { firebase };
+//const app = initializeApp(firebaseConfig);
+//const auth = getAuth(app);
