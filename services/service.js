1725dd61d83e5cde2748e21d858b8df8ef2f1763
drkchn <57195399+drkchn@users.noreply.github.com>
2024-01-09 06:54:49 -0800

    Queries Google Firestore database, searches, filters consultants by parameters

--- services/service.js
+++ services/service.js
@@ -3,29 +3,34 @@ const firebase = require('firebase/app');
 require('firebase/firestore');
 const config = require('./firebase/config.js');
 
-firebase.initializeApp(config);
+if (!firebase.apps.length) {
+	firebase.initializeApp(config);
+}
 
-// create some variable db to access the firestore database
+// create some variable db to access the Firestore database
 const db = firebase.firestore();
 
-// function to search consultants by name
-// takes in a name and returns a list of consultants
+/**
+ * Searches for consultants in a Google Firestore database by name
+ *
+ * @async
+ * @function searchConsultants
+ * @param {string} name The name of the consultant to search for
+ * @returns {Promise<Array<Object>>} A promise that resolves to an array of consultant objects
+ * @throws {Error} Will throw an error if the search operation fails
+ */
 async function searchConsultants(name) {
 	try {
-		let query = db.collection('people');
+		let results = [];
 
 		if (name) {
-			query = query.where('name', 'array-contains', name);
+			// we create a query for consultants with the given name
+			const snapshot = await db
+				.collection('Consultants')
+				.where('name', '==', name)
+				.get();
+			snapshot.forEach((doc) => results.push(doc.data()));
 		}
-
-		// run the query
-		const snapshot = await query.get();
-
-		// process the results
-		const results = [];
-		snapshot.forEach((doc) => {
-			results.push(doc.data());
-		});
 
 		return results;
 	} catch (error) {
@@ -34,6 +39,22 @@ async function searchConsultants(name) {
 	}
 }
 
+/**
+ * Filters consultants from a Google Firestore database based on given parameters
+ * The parameters will be used in the following logical operation:
+ * (speciality1 OR speciality2 OR ...)
+ * AND yearsOfExperience >= minYearsOfExperience
+ * AND (region1 OR region2 OR ...)
+ *
+ * @async
+ * @function filterConsultants
+ * @param {Array<string>} specialities The specialities to filter by
+ * @param {number} minYearsOfExperience The minimum years of experience to filter by
+ * @param {Array<string>} regions The regions to filter by
+ * @param {number} [maxPrice] The maximum price to filter by (optional)
+ * @returns {Promise<Array<Object>>} A promise that resolves to an array of consultant objects that match the parameters
+ * @throws {Error} Will throw an error if the query operation fails
+ */
 async function filterConsultants(
 	specialities,
 	minYearsOfExperience,
@@ -41,42 +62,31 @@ async function filterConsultants(
 	maxPrice,
 ) {
 	try {
+		let query = db.collection('Consultants');
+
+		// we create the query
+		const snapshot = await query.get();
+
 		let results = [];
+		// process the results
+		snapshot.forEach((doc) => {
+			let data = doc.data();
 
-		for (let speciality of specialities) {
-			let query = db
-				.collection('Consultants')
-				.where('speciality', 'array-contains-any', speciality);
-
-			if (minYearsOfExperience) {
-				query = query.where(
-					'yearsOfExperience',
-					'>=',
-					minYearsOfExperience,
-				);
-			}
-
-			if (regions) {
-				query = query.where('regions', 'array-contains-any', regions);
-			}
-
-			/* maxPrice in-progress
-                if (maxPrice) {
-                    query = query.where('price', '<=', maxPrice);
-                }
-				*/
-
-			// run the query
-			const snapshot = await query.get();
-
-			// process the results
-			snapshot.forEach((doc) => {
-				// we add consultants if they are not already in the results
+			// check if the consultant matches all the criteria
+			if (
+				specialities.some((speciality) =>
+					data.speciality.includes(speciality),
+				) &&
+				data.yearsOfExperience >= minYearsOfExperience &&
+				regions.some((region) => data.regions.includes(region))
+			) {
+				// we add consultants to our return array only if they are not already in the results
+				// this prevents duplicate consultants from being added to the array
 				if (!results.find((result) => result.id === doc.id)) {
-					results.push(doc.data());
+					results.push({ id: doc.id, ...data });
 				}
-			});
-		}
+			}
+		});
 
 		return results;
 	} catch (error) {
@@ -84,3 +94,5 @@ async function filterConsultants(
 		throw error;
 	}
 }
+
+module.exports = { searchConsultants, filterConsultants };
