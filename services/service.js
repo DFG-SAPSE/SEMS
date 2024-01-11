8ca16935780afb1c8c1b124d41430b3f2c299b7d
drkchn <57195399+drkchn@users.noreply.github.com>
2024-01-10 16:11:14 -0800

    Query for server side filtering, changed to ES6 syntax

--- services/service.js
+++ services/service.js
@@ -1,7 +1,7 @@
 // import firebase and set up using the config.js file
-const firebase = require('firebase/app');
-require('firebase/firestore');
-const config = require('./firebase/config.js');
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import config from './firebase/config.js';
 
 if (!firebase.apps.length) {
 	firebase.initializeApp(config);
@@ -41,7 +41,7 @@ async function searchConsultants(name) {
 
 /**
  * Filters consultants from a Google Firestore database based on given parameters
- * The parameters will be used in the following logical operation:
+ * The filtering operation is:
  * (speciality1 OR speciality2 OR ...)
  * AND yearsOfExperience >= minYearsOfExperience
  * AND (region1 OR region2 OR ...)
@@ -55,7 +55,7 @@ async function searchConsultants(name) {
  * @returns {Promise<Array<Object>>} A promise that resolves to an array of consultant objects that match the parameters
  * @throws {Error} Will throw an error if the query operation fails
  */
-async function filterConsultants(
+async function filterConsultantsClientSide(
 	specialities,
 	minYearsOfExperience,
 	regions,
@@ -95,4 +95,79 @@ async function filterConsultants(
 	}
 }
 
-module.exports = { searchConsultants, filterConsultants };
+/**
+ * Performs a Firestore query to filter consultants based on provided criteria
+ * The filtering operation is: 
+ * (speciality1) 
+ * AND (yearsOfExperience >= minYearsOfExperience) 
+ * AND (region1 OR region2 OR ...)
+ *
+ * @async
+ * @param {string[]} specialities An array of specialities to filter by. The first speciality in the array is used for filtering
+ * @param {number} minYearsOfExperience The minimum years of experience to filter by
+ * @param {string[]} regions An array of regions to filter by. If the array has more than 10 elements, we split into chunks of 10 and multiple queries are performed
+ * @returns {Promise<Object[]>} A promise that resolves to an array of consultant objects that match the parameters
+ * @throws {Error} Will throw an error if the query operation fails
+ */async function filterConsultantsServerSide(
+	specialities,
+	minYearsOfExperience,
+	regions,
+) {
+	try {
+		let query = db.collection('Consultants');
+
+		// we create the query
+		if (specialities.length > 0) {
+			// we only use the first speciality in the array for filtering
+			// this is because Firestore does not support array-contains-any for multiple fields
+			// this is a limitation of Firestore queries server side filtering
+			query = query.where('specialty', '==', specialities[0]);
+		}
+
+		if (minYearsOfExperience) {
+			query = query.where('experienceYears', '>=', minYearsOfExperience);
+		}
+
+		let results = [];
+		// if regions array argument has more than 10 elements, we need to split it into chunks of 10 and perform multiple queries
+		if (regions.length > 10) {
+			const chunkSize = 10;
+			for (let i = 0; i < regions.length; i += chunkSize) {
+				const chunk = regions.slice(i, i + chunkSize);
+
+				const snapshot = await query
+					.where('geographic_regions', 'array-contains-any', chunk)
+					.get();
+				snapshot.forEach((doc) => {
+					let data = doc.data();
+					// avoid duplicates
+					if (!results.find((result) => result.id === doc.id)) {
+						results.push({ id: doc.id, ...data });
+					}
+				});
+			}
+		} else {
+			const snapshot = await query
+				.where('geographic_regions', 'array-contains-any', regions)
+				.get();
+			snapshot.forEach((doc) => {
+				let data = doc.data();
+				// avoid duplicates
+				if (!results.find((result) => result.id === doc.id)) {
+					results.push({ id: doc.id, ...data });
+				}
+			});
+		}
+
+		return results;
+	} catch (error) {
+		console.error('Error filtering for consultants:', error);
+		throw error;
+	}
+}
+
+export {
+	searchConsultants,
+	filterConsultantsClientSide,
+	filterConsultantsServerSide,
+};
