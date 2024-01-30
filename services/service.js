7b31a7b614c59f29277aa0fe1f2fce871ce02965
drkchn <57195399+drkchn@users.noreply.github.com>
2024-01-30 13:38:04 -0800

    Removed client-side filtering function

--- services/service.js
+++ services/service.js
@@ -39,61 +39,6 @@ async function searchConsultants(name) {
 	}
 }
 
-/**
- * Filters consultants from a Google Firestore database based on given parameters
- * The filtering operation is:
- * (speciality1 OR speciality2 OR ...)
- * AND yearsOfExperience >= minYearsOfExperience
- * AND (region1 OR region2 OR ...)
- *
- * @async
- * @function filterConsultants
- * @param {Array<string>} specialities The specialities to filter by
- * @param {number} minYearsOfExperience The minimum years of experience to filter by
- * @param {Array<string>} regions The regions to filter by
- * @param {number} [maxPrice] The maximum price to filter by (optional)
- * @returns {Promise<Array<Object>>} A promise that resolves to an array of consultant objects that match the parameters
- * @throws {Error} Will throw an error if the query operation fails
- */
-async function filterConsultantsClientSide(
-	specialities,
-	minYearsOfExperience,
-	regions,
-	maxPrice,
-) {
-	try {
-		let query = db.collection('Consultants');
-
-		// we create the query
-		const snapshot = await query.get();
-
-		let results = [];
-		// process the results
-		snapshot.forEach((doc) => {
-			let data = doc.data();
-
-			// check if the consultant matches all the criteria
-			if (
-				specialities.some((speciality) =>
-					data.speciality.includes(speciality),
-				) &&
-				data.yearsOfExperience >= minYearsOfExperience &&
-				regions.some((region) => data.regions.includes(region))
-			) {
-				// we add consultants to our return array only if they are not already in the results
-				// this prevents duplicate consultants from being added to the array
-				if (!results.find((result) => result.id === doc.id)) {
-					results.push({ id: doc.id, ...data });
-				}
-			}
-		});
-
-		return results;
-	} catch (error) {
-		console.error('Error filtering for consultants:', error);
-		throw error;
-	}
-}
 
 /**
  * Performs a Firestore query to filter consultants based on provided criteria
